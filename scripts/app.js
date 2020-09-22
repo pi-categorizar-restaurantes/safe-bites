feb944337f88232abfeb43401ce2135cebab09c5
Matheus Matsukawa <matheusmatsu@gmail.com>
2020-09-22 18:26:28 -0300

    :construction: Add prototype routes

--- scripts/app.js
+++ scripts/app.js
@@ -3,10 +3,20 @@
 (function () {
   function init() {
     const router = new Router([
-      new Route('index', 'index.html', true),
-      new Route('test1', 'test1.html'),
-      new Route('test2', 'test2.html'),
+      new Route('index', 'app.html', true),
+      new Route('sign-up-1', 'signUp1.html'),
+      new Route('sign-up-2-pf', 'signUp2PF.html'),
+      new Route('sign-up-2-pj', 'signUp2PJ.html'),
+      new Route('email-confirmation', 'emailConfirmation.html'),
+      new Route('post-register-login', 'postRegisterLogin.html'),
+      new Route('login', 'login.html'),
+      new Route('require-login', 'requireLogin.html'),
     ]);
   }
   init();
+
+  if(!location.hash) {
+    location.hash = 'index' 
+  }
+  
 }());
