cb7128b103a303298460efdfa9ca3f573e8c5b39
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-10-05 13:10:52 -0300

    Add Folder Structure with examples

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ExampleComponent from "../../components/ExampleComponent";
+
+const HomePage = () => {
+  return (
+    <div>
+      <ExampleComponent />
+    </div>
+  );
+};
+
+export default HomePage;
