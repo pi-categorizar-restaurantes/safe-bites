a01ce0709167aba948ea0f3bbbce3b9d385aa3de
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-10-07 16:42:34 -0300

    Onboarding page done. Need to fix carousel animate

---
+++ src/pages/Onboarding/index.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import CustomCarousel from "../../components/CustomCarousel";
+import Header from "../../components/Header";
+import onboardingSlides from "./onboardingSlides";
+import carouselSettings from "./carouselSettings";
+import "./styles.css";
+
+const Onboarding = () => {
+  useEffect(() => {
+    localStorage.setItem("first-access", false);
+  }, []);
+
+  return (
+    <div className="onboarding-container">
+      <Header />
+      <CustomCarousel
+        slides={onboardingSlides}
+        settings={carouselSettings}
+        indicator={true}
+        skippable={true}
+        skipTo="/home"
+      />
+    </div>
+  );
+};
+
+export default Onboarding;
---
+++ src/pages/Onboarding/onboardingSlides.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import placeIcon from "../../assets/estabelecimento.png";
+import mapIcon from "../../assets/mapa_icone.png";
+import UserReview from "../../components/UserReview";
+import mockReview from "../../mock/reviewMock";
+
+export default [
+  {
+    slideContent: (
+      <div className="onboarding-slide" key={1}>
+        <p>
+          Seja bem vindo ao Safe Bites, a plataforma ideal para amantes de
+          comida com restrições alimentares e estabelecimentos que desejam se
+          conectar a esses seletos clientes.
+        </p>
+      </div>
+    ),
+  },
+  {
+    slideContent: (
+      <div className="onboarding-slide" key={2}>
+        <p>
+          Se você é cliente, procure no mapa ou na lista um restaurante próximo
+          a você que ofereça os produtos que estão de acordo com sua dieta.
+        </p>
+        <div className="onboarding-icons-container">
+          <span>
+            <img src={mapIcon} alt="Ícone de mapa" />
+          </span>
+          <span>
+            <img src={placeIcon} alt="Ícone de estabelecimento" />
+          </span>
+        </div>
+      </div>
+    ),
+  },
+  {
+    slideContent: (
+      <div className="onboarding-slide" key={3}>
+        <p>
+          {" "}
+          Se você é estabelecimento, descubra como melhorar seu atendimento para
+          clientes com restrições alimentares.
+        </p>
+        <UserReview reviewData={mockReview} />
+      </div>
+    ),
+  },
+  {
+    slideContent: (
+      <div className="onboarding-slide" key={4}>
+        <p>
+          Gostou? Comece agora mesmo a utilizar nossa plataforma! Cadastre-se
+          para ter acesso a todas as nossas funcionalidades.
+        </p>
+        <div className="onboarding-buttons-container">
+          <Link to={""} className="default-button green title-font">
+            Quero me cadastrar
+          </Link>
+          <Link to={""} className="default-button red title-font">
+            Prosseguir sem cadastro
+          </Link>
+        </div>
+      </div>
+    ),
+  },
+];
