c2f8c5a68a5e9376e0fa32f778dc76fc3cefdc59
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-10-06 21:11:57 -0300

    Update review mock with reviewId field

--- src/mock/reviewMock.js
+++ src/mock/reviewMock.js
@@ -1,6 +1,10 @@
 export default {
-  userAvatarUrl: "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=644&q=80", userName: "João das Neves",
-  rating: 3,
-  reviewText: "Restaurante cuidadoso com seus clientes. O garçom me perguntou se eu gostaria que o tempero viesse separado. Foi ótimo, minha saúde agradece. Recomendo para quem tem restrição de sódio. Mas não passa cartão refeição, venha preparado.",
-  isLiked=false
-}
+  reviewId: "fdDw123m0",
+  userAvatarUrl:
+    "https://images.unsplash.com/photo-1552058544-f2b08422138a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=644&q=80",
+  userName: "João das Neves",
+  rating: 4.0,
+  reviewText:
+    "Restaurante cuidadoso com seus clientes. O garçom me perguntou se eu gostaria que o tempero viesse separado. Foi ótimo, minha saúde agradece. Recomendo para quem tem restrição de sódio. Mas não passa cartão refeição, venha preparado.",
+  isLiked: false,
+};
