5adb27b3e46a11e29376b7a573c26d4055d2900a
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-10-08 13:51:38 -0300

    Add Intermediary Page, SignUp, routes for testing

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import IntermediarySignUp from "./pages/IntermediarySignUp";
 import Onboarding from "./pages/Onboarding";
 import Page404 from "./pages/Page404";
 import SignUp from "./pages/SignUp";
@@ -17,10 +18,19 @@ const Routes = () => {
         {firstAccess === "false" && (
           <Route path="/" exact={true} component={HomePage} />
         )}
-        <Route path="/home" component={HomePage} />
-        <Route path="/onboarding" component={Onboarding} />
-        <Route path="/signup/place" render={() => <SignUp isPlace={true} />} />
-        <Route path="/signup/user" render={() => <SignUp isPlace={false} />} />
+        <Route path="/home" exact={true} component={HomePage} />
+        <Route path="/onboarding" exact={true} component={Onboarding} />
+        <Route path="/signup" exact={true} component={IntermediarySignUp} />
+        <Route
+          path="/signup/place"
+          exact={true}
+          render={() => <SignUp isPlace={true} />}
+        />
+        <Route
+          path="/signup/user"
+          exact={true}
+          render={() => <SignUp isPlace={false} />}
+        />
         <Route path="*" component={Page404} />
       </Switch>
     </BrowserRouter>
