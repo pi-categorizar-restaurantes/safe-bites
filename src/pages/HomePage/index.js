74a864ccacf4227833d2520e8f44de6dd0108c5e
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-10-15 05:07:34 -0300

    Alter Home Page from route buttons to actual home

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,31 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import FooterMenu from "../../components/FooterMenu";
 import Header from "../../components/Header";
+import RestaurantList from "../../components/RestaurantList";
 import "./styles.css";
 
 const HomePage = () => {
+  const [view, setView] = useState("mapa");
   return (
     <>
       <Header isMainPage={true} />
-      <section className="routes">
-        {" "}
-        <h1 className="custom-title-font">ROTAS ATÉ AGORA</h1>
-        <Link to="/" className="custom-default-button custom-green">
-          Onboarding / Home
-        </Link>
-        <Link to="/onboarding" className="custom-default-button custom-green">
-          Onboarding
-        </Link>
-        <Link to="/signup" className="custom-default-button custom-green">
-          Página Intermediária de Cadastro
-        </Link>
-        <Link to="/signup/place" className="custom-default-button custom-green">
-          Cadastro Restaurante
-        </Link>
-        <Link to="/signup/user" className="custom-default-button custom-green">
-          Cadastro Usuário
-        </Link>
+      <section className="custom-main-page-container">
+        {view === "lista" && <RestaurantList />}
       </section>
+      <FooterMenu toRender={view} setRender={setView} />
     </>
   );
 };
